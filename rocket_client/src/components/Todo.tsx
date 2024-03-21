3c3a6fbcd1a09e9477ef9d3b4c7a1000f927fc52
Amal221200 <amalmurikkoli2000@gmail.com>
2024-03-21 08:22:10 +0530

    dnd bug fixes

--- rocket_client/src/components/Todo.tsx
+++ rocket_client/src/components/Todo.tsx
@@ -26,21 +26,20 @@ const Todo: React.FC<TodoComp> = ({ todo }) => {
 
     const handleDelete = useCallback((e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
         e.stopPropagation()
-        alert("Deletin");
 
         triggerDelete({ id: todo._id.$oid })
     }, [todo, triggerDelete])
 
     return (
-        <article ref={setNodeRef} {...attributes} {...listeners} style={{ ...style, touchAction: 'none' }} onClick={handleCheck}
-            className="bg-white/10 px-2 py-2 rounded cursor-pointer group/todo">
-            <div className="flex justify-start flex-row items-center gap-3">
-                <CheckBox key={todo._id.$oid} handleCheck={handleCheck} checked={todo.completed} />
+        <article style={{ ...style, touchAction: 'none' }} onClick={handleCheck}
+            className={cn("bg-white/10 px-2 py-2 rounded cursor-pointer group/todo", "flex justify-start flex-row items-center gap-3")}>
+            <CheckBox key={todo._id.$oid} handleCheck={handleCheck} checked={todo.completed} />
+            <div ref={setNodeRef} {...attributes} {...listeners} className="flex-1">
                 <h4 className={cn("text-gray-200 flex-1", todo.completed && "line-through text-gray-200/50")}>{todo.body}</h4>
-                <button type="button" onClick={handleDelete} className="z-10">
-                    <X className={cn("text-white opacity-0 group-hover/todo:opacity-100")} />
-                </button>
             </div>
+            <button type="button" onClick={handleDelete} className="z-10">
+                <X className={cn("text-white opacity-0 group-hover/todo:opacity-100")} />
+            </button>
         </article>
     )
 }
